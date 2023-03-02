05fe22231646b18a70910e75f159489caac01e10
CatJelly <mej1406@gmail.com>
2023-03-02 09:21:06 +0900

    first step complete

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -1,7 +1,28 @@
 package org.example;
 
+/*
+* 자바 명언 앱 자동 생성기 프로젝트
+ */
+
+import java.util.Scanner;
+
 public class Main {
     public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
 
+        System.out.println("== 명언 앱 ==");
+
+        while(true) {
+            System.out.print("명령) ");
+
+            String command = sc.nextLine();
+
+            if(command.equals("종료")) {
+                break;
+            }
+
+        }
+
+        sc.close();
     }
 }
