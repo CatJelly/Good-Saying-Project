14d1ee62158c64895405dc8a8db3ec79def5d5b5
CatJelly <mej1406@gmail.com>
2023-03-02 10:09:58 +0900

    6th step complete

--- src/main/java/org/example/App.java
+++ src/main/java/org/example/App.java
@@ -1,16 +1,14 @@
 package org.example;
 
-import java.util.ArrayList;
-import java.util.List;
-import java.util.Scanner;
+import java.util.*;
 
 public class App {
     static int sayingNum = 1;
-    private List<GoodSaying> list;
+    private Map<Integer, GoodSaying> map;
 
 
     public void init() {
-        list = new ArrayList<>();
+        map = new HashMap<>();
     }
 
     public void showTitle() {
@@ -19,15 +17,29 @@ public class App {
 
     public GoodSaying addSaying(String author, String saying) {
         GoodSaying temp = new GoodSaying(author, saying);
-        list.add(temp);
+        map.put(sayingNum, temp);
         sayingNum++;
         return temp;
     }
 
     public void printSaying() {
         System.out.println("번호\t/ 작가\t/ 명언");
-        for(GoodSaying gs : list) {
+        for(GoodSaying gs : map.values()) {
             System.out.println(gs);
+        }
+    }
+
+    public GoodSaying checkId(int id) {
+        return map.get(id);
+    }
+    public void deleteSaying(int id) {
+        GoodSaying deleteObj = checkId(id);
+        if(deleteObj == null) {
+            System.out.printf("%d번 명언은 존재하지 않습니다.\n", id);
+        }
+        else {
+            map.remove(id);
+            System.out.printf("%d번 명언이 삭제되었습니다.\n", id);
         }
     }
 
@@ -55,6 +67,10 @@ public class App {
             }
             else if(command.equals("목록")) {
                 printSaying();
+            }
+            else if(command.contains("삭제")) {
+                String [] split = command.split("\\?|=");
+                deleteSaying(Integer.parseInt(split[2]));
             }
         }
 
