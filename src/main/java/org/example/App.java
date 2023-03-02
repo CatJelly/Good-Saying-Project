a30c142edab23eef10bb499e79187f1d7cf2baa9
CatJelly <mej1406@gmail.com>
2023-03-02 13:15:58 +0900

    10th step complete

--- src/main/java/org/example/App.java
+++ src/main/java/org/example/App.java
@@ -1,13 +1,14 @@
 package org.example;
 
 import org.example.goodSaying.entity.GoodSaying;
+import org.json.JSONArray;
+import org.json.JSONObject;
 
 import java.io.*;
 import java.util.*;
 
 public class App {
     static final String FILE_PATH = "data.txt";
-
     static int sayingNum = 1;
     private Scanner sc;
     private Map<Integer, GoodSaying> map;
@@ -17,19 +18,19 @@ public class App {
     private BufferedReader br;
     private BufferedWriter bw;
 
-
     public void init() {
         map = new HashMap<>();
         sc = new Scanner(System.in);
         file = new File(FILE_PATH);
 
         try {
-            if(!file.exists()) {
+            if (!file.exists()) {
                 file.createNewFile();
             }
             fr = new FileReader(file);
             fw = new FileWriter(file, true);
-        } catch (IOException e) { }
+        } catch (IOException e) {
+        }
 
         br = new BufferedReader(fr);
         bw = new BufferedWriter(fw);
@@ -42,7 +43,8 @@ public class App {
             br.close();
             fw.close();
             fr.close();
-        } catch (IOException e) { }
+        } catch (IOException e) {
+        }
     }
 
     public void showTitle() {
@@ -101,7 +103,7 @@ public class App {
     }
 
     public void writeSaying() {
-        for(GoodSaying gs : map.values()) {
+        for (GoodSaying gs : map.values()) {
             String res = String.format("%d\t%s\t%s\n", gs.getId(), gs.getAuthor(), gs.getSaying());
             try {
                 bw.write(res);
@@ -114,18 +116,32 @@ public class App {
     public void readSaying() {
         String line = "";
         int id = 1;
-        while(true) {
+        while (true) {
             try {
                 if ((line = br.readLine()) == null)
                     break;
-            } catch (IOException e) { }
-            String [] split = line.split("\t");
+            } catch (IOException e) {
+            }
+            String[] split = line.split("\t");
             id = Integer.parseInt(split[0]);
             map.put(id, new GoodSaying(id, split[1], split[2]));
         }
         App.sayingNum = id + 1;
     }
 
+    public void build() {
+        try {
+            BufferedWriter jsonBw = new BufferedWriter(new FileWriter(new File("data.json")));
+            JSONObject json = new JSONObject();
+            List<GoodSaying> list = map.values().stream().toList();
+            JSONArray jsonArray = new JSONArray();
+            jsonArray.put(list);
+            jsonBw.write(jsonArray.toString());
+            jsonBw.close();
+        } catch (IOException e) {
+            throw new RuntimeException(e);
+        }
+    }
 
     public void run() {
         init();
@@ -146,18 +162,19 @@ public class App {
 
                 GoodSaying temp = addSaying(author, saying);
                 System.out.printf("%d번 명언이 등록되었습니다.\n", temp.getId());
-            }
-            else if (command.equals("목록")) {
+            } else if (command.equals("목록")) {
                 printSaying();
-            }
-            else if (command.contains("삭제")) {
+            } else if (command.contains("삭제")) {
                 String[] split = command.split("\\?|=");
                 deleteSaying(Integer.parseInt(split[2]));
-            }
-            else if(command.contains("수정")) {
+            } else if (command.contains("수정")) {
                 String[] split = command.split("\\?|=");
                 modifySaying(Integer.parseInt(split[2]));
+            } else if(command.equals("빌드")) {
+                build();
+                System.out.println("data.json 파일의 내용이 갱신되었습니다.");
             }
+
         }
 
         writeSaying();
