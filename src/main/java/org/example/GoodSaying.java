34c30afdea3e1c623c54b2cd514128c32e9daa3e
CatJelly <mej1406@gmail.com>
2023-03-02 09:30:46 +0900

    ãsecond step complete

---
+++ src/main/java/org/example/GoodSaying.java
@@ -0,0 +1,19 @@
+package org.example;
+
+public class GoodSaying {
+    private String author;
+    private String saying;
+
+    public GoodSaying(String author, String saying) {
+        this.author = author;
+        this.saying = saying;
+    }
+
+    public String getAuthor() {
+        return author;
+    }
+
+    public String getSaying() {
+        return saying;
+    }
+}
--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -1,26 +1,34 @@
 package org.example;
 
 /*
-* 자바 명언 앱 자동 생성기 프로젝트
+ * 자바 명언 앱 자동 생성기 프로젝트
  */
 
+import java.util.ArrayList;
+import java.util.List;
 import java.util.Scanner;
 
 public class Main {
     public static void main(String[] args) {
+        List<GoodSaying> list = new ArrayList<>();
         Scanner sc = new Scanner(System.in);
 
         System.out.println("== 명언 앱 ==");
 
-        while(true) {
+        while (true) {
             System.out.print("명령) ");
 
             String command = sc.nextLine();
 
-            if(command.equals("종료")) {
+            if (command.equals("종료")) {
                 break;
+            } else if (command.equals("등록")) {
+                System.out.print("명언 : ");
+                String saying = sc.nextLine();
+                System.out.print("작가 : ");
+                String author = sc.nextLine();
+                list.add(new GoodSaying(author, saying));
             }
-
         }
 
         sc.close();
